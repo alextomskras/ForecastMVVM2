63e88ff0869140344df254708a7c39ee4c3d4718
FESS <dreamer.alex.toms@gmail.com>
2019-01-13 15:41:59 +0300

    forecast-entity-day-format-change-totalprecip_in-from-Int-to-Double
    Signed-off-by: FESS <dreamer.alex.toms@gmail.com>

--- app/src/main/java/com/example/forecastmvvm/data/db/entity/Day.kt
+++ app/src/main/java/com/example/forecastmvvm/data/db/entity/Day.kt
@@ -27,8 +27,8 @@ data class Day(
     @SerializedName("mintemp_f")
     val mintempF: Double,
     @SerializedName("totalprecip_in")
-    val totalprecipIn: Int,
+    val totalprecipIn: Double,
     @SerializedName("totalprecip_mm")
-    val totalprecipMm: Int,
+    val totalprecipMm: Double,
     val uv: Double
 )
