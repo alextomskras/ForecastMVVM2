0af453d0d6469c7dd2e564fdcb8a961992ab5b01
FESS <dreamer.alex.toms@gmail.com>
2019-10-19 22:16:55 +0300

    check-icon-and-api-responce-openweather-api-v1
    Signed-off-by: fess <dreamer.alex.toms@gmail.com>

--- app/src/main/java/com/example/forecastmvvm/data/network/ApixuWeatherApiService.kt
+++ app/src/main/java/com/example/forecastmvvm/data/network/ApixuWeatherApiService.kt
@@ -11,18 +11,28 @@ import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
 
-
+const val API_KEY = "f9d142fda1995a149d1b35f11bc9a928"
 //OLD-const val API_KEY = "e3b4ae74af25401a849181235180512"
-const val API_KEY = "3e735f3d35d2f20f7604151dd0742757"
+//NEW_WEATHERCAST_const val API_KEY = "3e735f3d35d2f20f7604151dd0742757"
 //API-KEY-NEW 3e735f3d35d2f20f7604151dd0742757
+
+
+//WEATHER_ICON
+//DAY-CLEAR-SKY-http://openweathermap.org/img/wn/01d@2x.png
+
+
+//https://api.openweathermap.org/data/2.5/forecast?q=London,us
+//https://samples.openweathermap.org/data/2.5/forecast?id=524901&appid=b1b15e88fa797225412429c1c50c122a1
+//http://api.weatherstack.com/current?access_key=3e735f3d35d2f20f7604151dd0742757&query=New%20York
+//http://api.weatherstack.com/current?key=3e735f3d35d2f20f7604151dd0742757&q=New%20York
 //http://api.apixu.com/v1/current.json?key=89e8bd89085b41b7a4b142029180210&q=London&lang=en
 //http://api.apixu.com/v1/current.json?key=e3b4ae74af25401a849181235180512&q=London&lang=en
 
 interface ApixuWeatherApiService {
 
-    @GET("current.json")
+    @GET("data/2.5/forecast")
     fun getCurrentWeather(
-        @Query("q") location: String,
+        @Query("query") location: String,
         @Query("lang") languageCode: String = "en"
     ): Deferred<CurrentWeatherResponse>
 
@@ -35,7 +45,7 @@ interface ApixuWeatherApiService {
                 val url = chain.request()
                     .url()
                     .newBuilder()
-                    .addQueryParameter("key", API_KEY)
+                    .addQueryParameter("appid", API_KEY)
                     .build()
                 val request = chain.request()
                     .newBuilder()
@@ -56,7 +66,7 @@ interface ApixuWeatherApiService {
 
             return Retrofit.Builder()
                 .client(okHttpClient)
-                .baseUrl("https://api.apixu.com/v1/")
+                .baseUrl("http://api.weatherstack.com/")
                 .addCallAdapterFactory(CoroutineCallAdapterFactory())
                 .addConverterFactory(GsonConverterFactory.create())
                 .build()
