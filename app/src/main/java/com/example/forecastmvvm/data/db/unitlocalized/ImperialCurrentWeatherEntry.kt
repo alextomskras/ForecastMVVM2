9111bbded716d73dee435d0dbe31efe900d679b3
FESS <dreamer.alex.toms@gmail.com>
2019-10-27 18:03:48 +0300

    try-nocho-json-converter-v6 change kotlinOptions {                jvmTarget = '1.8'            }
    Signed-off-by: FESS <dreamer.alex.toms@gmail.com>

--- app/src/main/java/com/example/forecastmvvm/data/db/unitlocalized/ImperialCurrentWeatherEntry.kt
+++ app/src/main/java/com/example/forecastmvvm/data/db/unitlocalized/ImperialCurrentWeatherEntry.kt
@@ -5,19 +5,39 @@ import androidx.room.ColumnInfo
 
 data class ImperialCurrentWeatherEntry(
     @ColumnInfo(name = "main_temp")
-    override val temperature: Double
-//    @ColumnInfo(name = "condition_text")
-//    override val conditionText: String,
-//    @ColumnInfo(name = "condition_icon")
-//    override val conditionIconUrl: String,
-//    @ColumnInfo(name = "windMph")
-//    override val windSpeed: Double,
-//    @ColumnInfo(name = "windDir")
-//    override val windDirection: String,
-//    @ColumnInfo(name = "precipIn")
-//    override val precipitationVolume: Double,
-//    @ColumnInfo(name = "feelslikeF")
-//    override val feelsLikeTemperature: Double,
-//    @ColumnInfo(name = "visMiles")
-//    override val visibilityDistance: Double
+    override val temperature: Double,
+    @ColumnInfo(name = "weather_description")
+    override val conditionText: String,
+    @ColumnInfo(name = "weather_icon")
+    override val conditionIconUrl: String = "openweathermap.org/img/wn/01d@2x.png",
+    @ColumnInfo(name = "wind_speed")
+    override val windSpeed: Double,
+    @ColumnInfo(name = "name")
+    override val windDirection: String,
+    @ColumnInfo(name = "main_pressure")
+    override val precipitationVolume: Double,
+    @ColumnInfo(name = "main_tempMax")
+    override val feelsLikeTemperature: Double,
+    @ColumnInfo(name = "main_humidity")
+    override val visibilityDistance: Double
 ) : UnitSpecificCurrentWeatherEntry
+
+
+//data class ImperialCurrentWeatherEntry(
+//    @ColumnInfo(name = "main_temp")
+//    override val temperature: Double
+////    @ColumnInfo(name = "condition_text")
+////    override val conditionText: String,
+////    @ColumnInfo(name = "condition_icon")
+////    override val conditionIconUrl: String,
+////    @ColumnInfo(name = "windMph")
+////    override val windSpeed: Double,
+////    @ColumnInfo(name = "windDir")
+////    override val windDirection: String,
+////    @ColumnInfo(name = "precipIn")
+////    override val precipitationVolume: Double,
+////    @ColumnInfo(name = "feelslikeF")
+////    override val feelsLikeTemperature: Double,
+////    @ColumnInfo(name = "visMiles")
+////    override val visibilityDistance: Double
+//) : UnitSpecificCurrentWeatherEntry
--- app/src/main/java/com/example/forecastmvvm/data/db/unitlocalized/MetricCurrentWeatherEntry.kt
+++ app/src/main/java/com/example/forecastmvvm/data/db/unitlocalized/MetricCurrentWeatherEntry.kt
@@ -5,19 +5,19 @@ import androidx.room.ColumnInfo
 
 data class MetricCurrentWeatherEntry(
     @ColumnInfo(name = "main_temp")
-    override val temperature: Double
-//    @ColumnInfo(name = "condition_text")
-//    override val conditionText: String,
-//    @ColumnInfo(name = "condition_icon")
-//    override val conditionIconUrl: String,
-//    @ColumnInfo(name = "windKph")
-//    override val windSpeed: Double,
-//    @ColumnInfo(name = "windDir")
-//    override val windDirection: String,
-//    @ColumnInfo(name = "precipMm")
-//    override val precipitationVolume: Double,
-//    @ColumnInfo(name = "feelslikeC")
-//    override val feelsLikeTemperature: Double,
-//    @ColumnInfo(name = "visKm")
-//    override val visibilityDistance: Double
+    override val temperature: Double,
+    @ColumnInfo(name = "weather_description")
+    override val conditionText: String,
+    @ColumnInfo(name = "weather_icon")
+    override val conditionIconUrl: String = "openweathermap.org/img/wn/01d@2x.png",
+    @ColumnInfo(name = "wind_speed")
+    override val windSpeed: Double,
+    @ColumnInfo(name = "name")
+    override val windDirection: String,
+    @ColumnInfo(name = "main_pressure")
+    override val precipitationVolume: Double,
+    @ColumnInfo(name = "main_tempMax")
+    override val feelsLikeTemperature: Double,
+    @ColumnInfo(name = "main_humidity")
+    override val visibilityDistance: Double
 ) : UnitSpecificCurrentWeatherEntry
